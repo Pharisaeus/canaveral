9ed9561e5ce55e6228e14b0ecbaf984624a2291e
pawelkaminski <pawel.kaminski@revolut.com>
2019-02-02 16:06:23 +0100

    * change name in config * add logging for configured properties

--- canaveral-mocks/canaveral-redis-mock/src/test/java/pl/codewise/canaveral/mock/redis/RedisMockProviderIntegrationTest.java
+++ canaveral-mocks/canaveral-redis-mock/src/test/java/pl/codewise/canaveral/mock/redis/RedisMockProviderIntegrationTest.java
@@ -1,14 +1,11 @@
 package pl.codewise.canaveral.mock.redis;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import pl.codewise.canaveral.core.runtime.RunnerContext;
 import redis.clients.jedis.Jedis;
-
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -27,7 +24,7 @@ class RedisMockProviderIntegrationTest {
         when(runnerContext.getFreePort()).thenCallRealMethod();
 
         redisMockProvider = RedisMockProvider.newConfig()
-                .overrideRedisVersion("redis:3.0.6")
+                .withRedisDockerImage("redis:3.0.6")
                 .registerHostUnder("my-property.redis.host")
                 .registerPortUnder("my-property.redis.port")
                 .build("redis-mock");
